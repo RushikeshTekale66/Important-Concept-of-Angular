f356fb604abff5db6d2530839805295892304b12
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-12-06 19:14:07 +0530

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataBindingComponent } from './Components/data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
