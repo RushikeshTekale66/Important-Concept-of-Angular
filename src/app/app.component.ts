c530d5ec677ebdc35465a56f5487c8613884498d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-12-19 13:10:21 +0530

    User Pipe & Builtin Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { DataBindingComponent } from './Components/data-binding/data-binding.com
 import {NavbarComponent} from "./navbar/navbar.component";
 import { FormModelComponent } from "./form-model/form-model.component";
 import {DirectivesComponent} from "./directives/directives.component";
+import {PipesComponent} from "./pipes/pipes.component";
+import {NewPipePipe} from "./new-pipe.pipe";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DirectivesComponent],
+  imports: [RouterOutlet, PipesComponent , NewPipePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
