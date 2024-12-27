cb2d0c76c0040b1aa94b00c264c00e4c5323399e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-12-27 13:08:13 +0530

    Services in angular

--- src/app/post-api/post-api.component.ts
+++ src/app/post-api/post-api.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DepartmentService } from '../service/department.service';
 
 @Component({
   selector: 'app-post-api',
@@ -15,14 +16,34 @@ export class PostApiComponent implements OnInit {
     'departmentLogo': ""
   }
 
+  //Two ways to create department service
+  // 1
   http = inject(HttpClient);
+
+  // 2
+  constructor(private deptSrv: DepartmentService) { };
 
   ngOnInit(): void {
     this.getDepartment();
   }
 
+  //Normal API Call
+  // onSave() {
+  //   this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment", this.deptObj).subscribe((res: any) => {
+  //     debugger;
+  //     if (res.result) {
+  //       alert("Department created successfully");
+  //       this.getDepartment();
+  //     }
+  //     else {
+  //       alert(res.message);
+  //     }
+  //   })
+  // }
+
+  // API Call using Service
   onSave() {
-    this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment", this.deptObj).subscribe((res: any) => {
+    this.deptSrv.saveNewDep(this.deptObj).subscribe((res: any) => {
       debugger;
       if (res.result) {
         alert("Department created successfully");
@@ -35,9 +56,18 @@ export class PostApiComponent implements OnInit {
   }
 
   departmentList: any[] = [];
+
+  //Normal Api cal
+  // getDepartment() {
+  //   this.http.get("https://projectapi.gerasim.in/api/Complaint/GetParentDepartment").subscribe((result: any) => {
+  //     this.departmentList = result.data;
+  //   })
+  // }
+
+  //Api call using Service
   getDepartment() {
-    this.http.get("https://projectapi.gerasim.in/api/Complaint/GetParentDepartment").subscribe((result: any) => {
-      this.departmentList = result.data;
+    this.deptSrv.getAllDept().subscribe((res: any) => {
+      this.departmentList = res.data;
     })
   }
 
@@ -45,8 +75,22 @@ export class PostApiComponent implements OnInit {
     this.deptObj = data;
   }
 
+  // Normal API call
+  // onUpdate() {
+  //   this.http.post("https://projectapi.gerasim.in/api/Complaint/UpdateDepartment", this.deptObj).subscribe((res: any) => {
+  //     if (res.result) {
+  //       alert("Department Updated Successfylly");
+  //       this.getDepartment();
+  //     }
+  //     else {
+  //       alert(res.message);
+  //     }
+  //   })
+  // }
+
+  // API call using service
   onUpdate() {
-    this.http.post("https://projectapi.gerasim.in/api/Complaint/UpdateDepartment", this.deptObj).subscribe((res: any) => {
+    this.deptSrv.updateDepartment(this.deptObj).subscribe((res: any) => {
       if (res.result) {
         alert("Department Updated Successfylly");
         this.getDepartment();
@@ -57,12 +101,31 @@ export class PostApiComponent implements OnInit {
     })
   }
 
+
+  //Normal Api call
+  // onDelete(id: number) {
+  //   debugger;
+  //   const isDelete = confirm("Are you want to delete record");
+  //   alert(id);
+  //   if (isDelete) {
+  //     this.http.delete("https://projectapi.gerasim.in/api/Complaint/DeletedepartmentBydepartmentId?departmentId=" + id).subscribe((res: any) => {
+  //       if (res.result) {
+  //         alert("Department Deleted Successfylly");
+  //         this.getDepartment();
+  //       }
+  //       else {
+  //         alert(res.message);
+  //       }
+  //     })
+  //   }
+  // }
+
+  // API Call using service
   onDelete(id: number) {
     debugger;
     const isDelete = confirm("Are you want to delete record");
-    alert(id);
     if (isDelete) {
-      this.http.delete("https://projectapi.gerasim.in/api/Complaint/DeletedepartmentBydepartmentId?departmentId=" + id).subscribe((res: any) => {
+      this.deptSrv.deleteDepartment(id).subscribe((res: any) => {
         if (res.result) {
           alert("Department Deleted Successfylly");
           this.getDepartment();
@@ -72,6 +135,5 @@ export class PostApiComponent implements OnInit {
         }
       })
     }
-
   }
 }
